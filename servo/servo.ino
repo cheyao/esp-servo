09487cdbe4242a3f738d3b01ab95953c6eb08f23
cheyao <94928179+cheyao@users.noreply.github.com>
2024-12-08 11:52:15 +0100

    Improveme performance

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,18 +1,22 @@
 #include <Servo.h>
  
 Servo servo;
- 
+byte lstate;
+
 void setup() {
     servo.attach(D4);
     servo.write(0);
-    delay(2000);
     pinMode(0, INPUT_PULLUP);
 }
  
 void loop() {
-    if (digitalRead(0) == LOW) {
-        servo.write(0);
-    } else {
-        servo.write(180);
+    byte state = digitalRead(0);
+    if (lstate != state) {
+	if (state == LOW) {
+		servo.write(0);
+	} else {
+		servo.write(180);
+	}
+	lstate = state;
     }
 }
